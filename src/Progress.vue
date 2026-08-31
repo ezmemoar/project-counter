c3227e5c549a2201c1c1988e2087aab1d6d3356b
Raquis <rizalpratama10010@gmail.com>
2026-08-31 09:31:00 +0700

    Update Progress.vue

--- src/Progress.vue
+++ src/Progress.vue
@@ -44,8 +44,10 @@ const websiteTasks = ref([
   { label: 'Halaman Blog', done: true },
   { label: 'Halaman Detail Blog', done: true },
   { label: 'Halaman Track Pengiriman', done: true },
+  { label: 'Penambahan section Antar Langsung', done: true },
   { label: 'Integrasi Track Pengiriman', done: false },
   { label: 'Optimisasi SEO', done: true },
+  { label: 'Penyesuaian konten', done: true },
   { label: 'Deployment', done: false },
 ])
 
